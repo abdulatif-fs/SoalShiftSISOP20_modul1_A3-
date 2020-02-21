30177da5cb0420d36ef430f027201295c9fa46e6
abdulatif-fs <61305028+abdulatif-fs@users.noreply.github.com>
2020-02-21 12:25:48 +0700

    Add files via upload

---
+++ prak1lat2a.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+< /dev/urandom tr -dc  A-Z-a-z-0-9 | head -c${1:-28};echo;
---
+++ prak1lat2b.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+file1=$1
+< /dev/urandom tr -dc A-Z-a-z-0-9 | fold -w28|head -n1 > "$file1".txt
+
